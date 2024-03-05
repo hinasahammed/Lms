f36b2a5e302396c88105cb7814c8890a9f3cf1b9
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2024-03-05 23:54:58 +0530

    somechanges

--- lib/res/components/enrolled_course.dart
+++ lib/res/components/enrolled_course.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:get/get.dart';
+import 'package:lms/res/components/enrolled_course_details.dart';
 import 'package:lms/res/components/no_favorite_item_ui.dart';
 import 'package:lms/res/components/shimmer_list_ui.dart';
 import 'package:shimmer/shimmer.dart';
@@ -38,67 +39,72 @@ class EnrolledCourse extends StatelessWidget {
               vertical: 10,
             ),
             children: snapshot.data!.docs.map((enrolledCourse) {
-              return Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 6),
-                  child: Row(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: CachedNetworkImage(
-                          width: Get.width * .2,
-                          height: Get.height * .1,
-                          imageUrl: enrolledCourse['imageurl'],
-                          fit: BoxFit.cover,
-                          placeholder: (context, url) => Shimmer.fromColors(
-                            baseColor: Colors.black.withOpacity(0.2),
-                            highlightColor: Colors.white54,
-                            enabled: true,
-                            child: Container(
-                              width: Get.width * .2,
-                              height: Get.height * .1,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15),
-                                color: Colors.black54,
+              return InkWell(
+                onTap: () {
+                  Get.to(() => const EnrolledCourseDetails());
+                },
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6),
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: CachedNetworkImage(
+                            width: Get.width * .2,
+                            height: Get.height * .1,
+                            imageUrl: enrolledCourse['imageurl'],
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) => Shimmer.fromColors(
+                              baseColor: Colors.black.withOpacity(0.2),
+                              highlightColor: Colors.white54,
+                              enabled: true,
+                              child: Container(
+                                width: Get.width * .2,
+                                height: Get.height * .1,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Colors.black54,
+                                ),
                               ),
                             ),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
                           ),
-                          errorWidget: (context, url, error) =>
-                              const Icon(Icons.error),
                         ),
-                      ),
-                      const Gap(10),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            Text(
-                              enrolledCourse['courseTitle'],
-                              style: theme.textTheme.bodyLarge!.copyWith(
-                                color: theme.colorScheme.onBackground,
+                        const Gap(10),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Text(
+                                enrolledCourse['courseTitle'],
+                                style: theme.textTheme.bodyLarge!.copyWith(
+                                  color: theme.colorScheme.onBackground,
+                                ),
                               ),
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: LinearProgressIndicator(
-                                    color: theme.colorScheme.primary,
-                                    value: .5,
-                                    borderRadius: BorderRadius.circular(5),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: LinearProgressIndicator(
+                                      color: theme.colorScheme.primary,
+                                      value: .5,
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
                                   ),
-                                ),
-                                const Gap(5),
-                                Text(
-                                  '60%',
-                                  style: theme.textTheme.bodyLarge!.copyWith(
-                                    color: theme.colorScheme.onBackground,
+                                  const Gap(5),
+                                  Text(
+                                    '60%',
+                                    style: theme.textTheme.bodyLarge!.copyWith(
+                                      color: theme.colorScheme.onBackground,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               );
--- lib/res/components/enrolled_course_details.dart
+++ lib/res/components/enrolled_course_details.dart
@@ -1,15 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class EnrolledCourseDetails extends StatefulWidget {
+  const EnrolledCourseDetails({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<EnrolledCourseDetails> createState() => _EnrolledCourseDetailsState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _EnrolledCourseDetailsState extends State<EnrolledCourseDetails> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(
+        'https://drive.google.com/uc?export=download&id=1e7HJNDphTdvRQCJSTTYKeSwjrU6A2kmD'))
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Video'),
+      ),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play();
+          });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
